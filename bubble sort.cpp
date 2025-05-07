1ad2eb29189fd27fc44b8b314306f56b7bfc1654
tizbit <enanyeenatye@gmail.com>
2025-05-07 10:23:17 -1200

    add bubble sort

--- bubble sort.cpp
+++ bubble sort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// Bubble Sort using pointers
+// Bubble Sort using pointer
 void bubbleSort(int* arr, int size) {
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
